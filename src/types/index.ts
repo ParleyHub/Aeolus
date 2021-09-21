3fdadef983a6d3148ac0a84ba6e3dceaa1500331
Hiep Nguyen <hiepnh610@gmail.com>
2021-09-21 16:14:15 +0700

    feat: add more icon

---
+++ src/types/index.ts
@@ -0,0 +1,17 @@
+export interface IIconProps extends HTMLSpanElement {
+  variant?: 'info' | 'success' | 'error' | 'warning' | 'default' | 'black';
+  size?:
+    | 'xs'
+    | 'sm'
+    | 'base'
+    | 'lg'
+    | 'xl'
+    | '2xl'
+    | '3xl'
+    | '4xl'
+    | '5xl'
+    | '6xl'
+    | '7xl'
+    | '8xl'
+    | '9xl';
+}
